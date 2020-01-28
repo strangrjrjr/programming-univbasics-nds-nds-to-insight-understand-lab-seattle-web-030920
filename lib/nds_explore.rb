e40e02ee1006ae4a476e2ddd971ec02974196a42
Roger Robertson Jr <rjrjr@pm.me>
2020-01-28 18:28:34 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,7 +8,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  directors_database[0].each do |element|
+  directors_database[0][0].each do |element|
     puts 
   end
 end
